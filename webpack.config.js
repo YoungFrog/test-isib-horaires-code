144691f36343bdb761c16492af6f2d82985594bc
Nicolas Richard <nrichard@he2b.be>
2022-06-10 11:54:38 +0200

    shake off unused timezones and locales during build process
    app.js gets reduced from 935 KiB to 521 KiB

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,16 @@
 const path = require('path')
+const MomentTimezoneDataPlugin = require('moment-timezone-data-webpack-plugin')
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin')
 
 module.exports = {
+  plugins: [
+    new MomentTimezoneDataPlugin({
+      matchZones: 'Europe/Brussels',
+      startYear: 2020,
+      endYear: (new Date().getFullYear()) + 10
+    }),
+    new MomentLocalesPlugin()
+  ],
   entry: './src/index.tsx',
   module: {
     rules: [
